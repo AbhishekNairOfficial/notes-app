ab9f76979e84c600d48d640f57c7ced0a0b063e4
Abhishek P <abhishek.p@piktorlabs.com>
2019-08-10 18:55:31 +0530

    Initial Commit

---
+++ __tests__/App-test.js
@@ -0,0 +1,22 @@
+/**
+ * @format
+ */
+
+import 'react-native';
+import React from 'react';
+import App from '../App';
+import { render } from 'react-native-testing-library';
+
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+
+it('renders correctly', () => {
+  renderer.create(<App />);
+});
+
+describe('Hello', () => {
+  it('renders the correct message', () => {
+    const { queryByText } = render(<App />);
+    expect(queryByText('App.js')).not.toBeNull();
+  });
+});
