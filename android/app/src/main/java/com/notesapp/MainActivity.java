c1ecc3e1a4cc8954d3774f366cc88c4c59ddff07
Abhishek P <abhishek.p@piktorlabs.com>
2019-08-20 22:52:37 +0530

    Renamed app on android

--- android/app/src/main/java/com/notesapp/MainActivity.java
+++ android/app/src/main/java/com/notesapp/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "notesapp";
+        return "NotesApp";
     }
     @Override
 protected ReactActivityDelegate createReactActivityDelegate() {
